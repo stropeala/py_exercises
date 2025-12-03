10256eb7d5a44bb6e48c8553d16fd45fd0d84ead
stropeala <petre.ispir2002@protonmail.com>
2025-12-04 01:20:01 +0200

    Create deck_game.py

---
+++ ex/ex_c7/deck_game.py
@@ -0,0 +1,12 @@
+# Tocmai ce am ajuns la masa, unde suntem 4 jucatori.
+# Fiecare trebuie sa primeasca cate 5 carti dintr-un set standard de 52, valorile fiind:
+# As, 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King
+# Scrieti un program care sa:
+# 1. Creeze deck-ul de carti si sa amestece cartile
+# Random.shuffle() – ce e? documentam codul
+# 2. Colectioneze numele fiecarui jucator intr-un dictionar
+# Modelul dictionarului: key – numele jucatorului, value – lista de carti
+# Numele se va introduce de la tastatura
+# Maxim 4 jucatori, minim 2 jucatori se pot inregistra
+# 3. In momentul in care se inregistreaza un nou jucator, automat va primi 5 carti random
+# 4. La final, printam elementele dictionarului, pe rand
