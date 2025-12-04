f8554dea248aab05a665759575529c2e30813ba3
stropeala <petre.ispir2002@protonmail.com>
2025-12-04 17:11:33 +0200

    Update deck_game.py

--- ex/ex_c7/deck_game.py
+++ ex/ex_c7/deck_game.py
@@ -10,3 +10,53 @@
 # Maxim 4 jucatori, minim 2 jucatori se pot inregistra
 # 3. In momentul in care se inregistreaza un nou jucator, automat va primi 5 carti random
 # 4. La final, printam elementele dictionarului, pe rand
+
+import random
+
+# def deck():
+#     simboluri = 'hearts', 'spades', 'diamonds', 'clubs'
+#     Ace = []
+#     Two = []
+#     Three = []
+#     Four = []
+#     Five = []
+#     Six = []
+#     Seven = []
+#     Eight = []
+#     Nine = []
+#     Ten = []
+#     Jack = []
+#     Queen = []
+#     King = []
+#     for i in simboluri:
+#         Ace.append('As de ' + str(i))
+#         As.append('As de ' + str(i))
+
+
+# def deck_game():
+#     symbols = ['hearts', 'spades', 'diamonds', 'clubs']
+#     cards = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King']
+#     deck = []
+#     for i in cards:
+#         for j in symbols:
+#             deck.append(i + ' of ' + j)
+
+#     deck_of_cards = {deck[i]:1 for i in range(4),
+#         deck[i]:2 for i in range(4, 8)}
+#     return deck_of_cards
+
+# print(deck_game())
+
+
+def deck_game():
+    cards = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King']
+    symbols = ['hearts', 'spades', 'diamonds', 'clubs']
+    deck_of_cards_values = {}
+    for value, card in enumerate(cards, start = 1):
+        for symbol in symbols:
+            deck_of_cards_values[f'{card} of {symbol}'] = value
+    deck_of_cards = list(deck_of_cards_values)
+    random.shuffle(deck_of_cards)
+    print(deck_of_cards)
+
+deck_game()
