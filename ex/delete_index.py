e627d32c66e547177f2a2e03d967216856dc744d
Petre Razvan <petre.ispir2002@protonmail.com>
2025-12-12 18:02:39 +0200

    Create delete_index.py

---
+++ ex/delete_index.py
@@ -0,0 +1,12 @@
+# 2. Se da codul de mai jos.
+# Folositi metoda corecta pentru:
+# - a sterge elementul ce se afla la indexul 5
+# - printati lista
+
+# code start
+my_list = [10, 10.5, 20, 30, "Python", "Java", "Ruby"]
+print(my_list[5])
+my_list.pop(5)
+print(my_list)
+
+# code end
