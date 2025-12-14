de22480e06c13d2d15737ca55163fce8ba3aa0f8
Petre Razvan <petre.ispir2002@protonmail.com>
2025-12-14 18:03:42 +0200

    Create 12.combine_set.py

---
+++ ex_lists_sets_tuples/12.combine_set.py
@@ -0,0 +1,13 @@
+# 12. Se da codul de mai jos.
+# Folositi metodele corecte pentru:
+# - a determina, si printa, elementele comune celor doua seturi
+# - a pune elementele celor doua seturi impreuna, intr-un nou set; printati setul nou
+
+# code start
+my_set1 = {1, 4, 6, 5, 9, 0, 8, 3, 2, 7, 11}
+my_set2 = {12, 9, 4, 2, 0, 6}
+print(my_set1 & my_set2)
+new_set = my_set1 | my_set2
+print(new_set)
+
+# code end
