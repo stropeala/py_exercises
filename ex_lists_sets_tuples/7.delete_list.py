761d82f8b575745ba001de2a11e23300d0b00502
Petre Razvan <petre.ispir2002@protonmail.com>
2025-12-14 18:04:05 +0200

    Create 7.delete_list.py

---
+++ ex_lists_sets_tuples/7.delete_list.py
@@ -0,0 +1,16 @@
+# 7. Se da codul de mai jos.
+# Folositi metoda corecta pentru:
+# - a sterge toate elementele din lista
+
+# code start
+my_list = [10, 10.5, 20, 30, 25.6, 19.25, 11.01, 29.99]
+
+# 1st method
+del my_list[:]
+print(my_list)
+
+# 2nd method
+# my_list.clear()
+# print(my_list)
+
+# code end
