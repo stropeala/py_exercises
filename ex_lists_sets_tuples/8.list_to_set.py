2708c61ecc153ca3a481f7def6512b7e4b81694a
Petre Razvan <petre.ispir2002@protonmail.com>
2025-12-14 18:04:06 +0200

    Create 8.list_to_set.py

---
+++ ex_lists_sets_tuples/8.list_to_set.py
@@ -0,0 +1,12 @@
+# 8. Se da codul de mai jos.
+# Folositi metoda corecta pentru:
+# - a converti lista intr-un set
+
+# code start
+my_list = [10, 10.5, 10, 30, 25.6, 10.5, 11.01, 29.99]
+my_list.sort()
+print(type(my_list), my_list)
+my_set = set(my_list)
+print(type(my_set), my_set)
+
+# code end
