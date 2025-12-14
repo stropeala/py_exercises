75df86c497d687d3b7046c0b6017157c607889f1
Petre Razvan <petre.ispir2002@protonmail.com>
2025-12-14 18:03:43 +0200

    Create 13.dif_set.py

---
+++ ex_lists_sets_tuples/13.dif_set.py
@@ -0,0 +1,10 @@
+# 13. Se da codul de mai jos.
+# Folositi metoda corecta pentru:
+# - a determina care elemente din setul my_set2 nu se regasesc in my_set1
+
+# code start
+my_set1 = {1, 4, 6, 5, 9, 0, 8, 3, 2, 7, 11}
+my_set2 = {12, 9, 4, 2, 0, 6}
+print(my_set2 - my_set1)
+
+# code end
