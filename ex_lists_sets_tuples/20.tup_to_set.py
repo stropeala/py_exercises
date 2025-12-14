d439711e98ee75c5984443bd7efcc29595cdf8e7
Petre Razvan <petre.ispir2002@protonmail.com>
2025-12-14 18:03:57 +0200

    Create 20.tup_to_set.py

---
+++ ex_lists_sets_tuples/20.tup_to_set.py
@@ -0,0 +1,21 @@
+# 20. Se da codul de mai jos.
+# Folositi metoda corecta pentru:
+#  - a converti tuple-ul intr-o set
+
+# code start
+my_tup = (
+    "Romania",
+    "Poland",
+    "Estonia",
+    "Bulgaria",
+    "Slovakia",
+    "Slovenia",
+    "Estonia",
+    "Romania",
+    "Hungary",
+    "Slovenia",
+)
+my_set = set(my_tup)
+print(my_set)
+
+# code end
