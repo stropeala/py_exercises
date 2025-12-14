1d6436729fb4d96c536c86933bbc72480807cacb
Petre Razvan <petre.ispir2002@protonmail.com>
2025-12-14 14:52:54 +0200

    Delete len_list.py

--- ex/len_list.py
+++
@@ -1,8 +0,0 @@
-# 1. Se da codul de mai jos.
-# Folositi metoda corecta pentru:
-# - a determina, si printa, lungimea listei
-
-# code start
-my_list = [10, 10.5, 20, 30, "Python", "Java", "Ruby"]
-print(len(my_list))
-# code end
