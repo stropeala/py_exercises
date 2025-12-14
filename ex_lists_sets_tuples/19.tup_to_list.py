70ad26f0099bd60ab75bcb67fe61ff6fe7ef39fc
Petre Razvan <petre.ispir2002@protonmail.com>
2025-12-14 18:03:54 +0200

    Create 19.tup_to_list.py

---
+++ ex_lists_sets_tuples/19.tup_to_list.py
@@ -0,0 +1,21 @@
+# 19. Se da codul de mai jos.
+# Folositi metoda corecta pentru:
+#  - a converti tuple-ul intr-o lista
+
+# code start
+my_tup = (
+    "Romania",
+    "Poland",
+    "Estonia",
+    "Bulgaria",
+    "Slovakia",
+    "Slovenia",
+    "Estonia",
+    "Romania",
+    "Hungary",
+    "Slovenia",
+)
+my_list = list(my_tup)
+print(my_list)
+
+# code end
