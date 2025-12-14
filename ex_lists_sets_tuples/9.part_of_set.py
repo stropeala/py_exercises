b0d7be50a2f80ffe86eade640dd1faec1f26e04e
Petre Razvan <petre.ispir2002@protonmail.com>
2025-12-14 18:04:08 +0200

    Create 9.part_of_set.py

---
+++ ex_lists_sets_tuples/9.part_of_set.py
@@ -0,0 +1,17 @@
+# 9. Se da codul de mai jos.
+# Folositi metoda corecta pentru:
+# - a verifica daca 10 face parte din set
+
+# code start
+my_set = {1, 2, 3, 4, 6, 7, 11, 29}
+
+# 1st method
+if 10 in my_set:
+    print("10 is part of the set")
+else:
+    print("10 is not part of the set")
+
+# 2nd method
+print(10 in my_set)
+
+# code end
