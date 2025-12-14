76bdeb2dd6a848f362053db346559d7a49433ec7
Petre Razvan <petre.ispir2002@protonmail.com>
2025-12-14 18:03:45 +0200

    Create 14.len_index_tuple.py

---
+++ ex_lists_sets_tuples/14.len_index_tuple.py
@@ -0,0 +1,12 @@
+# 14. Se da codul de mai jos.
+# Folositi metodele corecte pentru:
+#  - a determina, si printa, care este lungimea colectiei de mai jos
+#  - a determina, si printa, care este indexul elementului "Poland"
+
+# code start
+my_tup = ("Romania", "Poland", "Estonia", "Bulgaria", "Slovakia", "Slovenia", "Hungary")
+print(my_tup)
+print(len(my_tup))
+print(my_tup.index("Poland"))
+
+# code end
