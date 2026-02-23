3e2416c9b93049b7a6052442e92b1728c912698a
Petre Razvan <petre.ispir2002@protonmail.com>
2026-02-23 16:20:10 +0200

    Create ex10.py

---
+++ ex_c21/ex10.py
@@ -0,0 +1,38 @@
+# 10. The following code is given.
+# Implement the decorator that will return the result of the function,
+# only if the result is bellow the set limit. If false, the decorator will
+# print out the "limit" parameter
+
+
+def max_result(limit):
+    def actual_wrapper(func):
+
+        def wrapper(*args):
+            func(*args)
+            result = func(*args)
+            if result > limit:
+                print(f"{result} > {limit}")
+            else:
+                print(f"{result} < {limit}")
+
+        return wrapper
+
+    return actual_wrapper
+
+
+@max_result(10)
+def square(number):
+    return number**2
+
+
+@max_result(50)
+def multiply(a, b):
+    return a * b
+
+
+if __name__ == "__main__":
+    squared_result = square(5)
+    print(f"Square result: {squared_result}")
+
+    multiplied_result = multiply(8, 7)
+    print(f"Multiplication result: {multiplied_result}")
