fada2a0b6df9fe3ef3749ed50e99bc2975b088d0
Petre Razvan <petre.ispir2002@protonmail.com>
2026-02-23 16:20:08 +0200

    Create ex1.py

---
+++ ex_c21/ex1.py
@@ -0,0 +1,22 @@
+# 1. Fix the code below so that it doesn't generate a SyntaxError.
+
+# Hint:  The result should be:
+
+# 20 200
+
+# or
+
+# 20
+# 200
+
+# code start
+
+x = [1, 2]
+y = [10, 100]
+
+for i in x:
+    for j in y:
+        if i % 2 == 0:
+            print(i * j)
+
+# code end
